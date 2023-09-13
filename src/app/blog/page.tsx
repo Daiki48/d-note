403e9a18699ecc42995d945405d19c7ffdd015f9
Daiki48 <daiki48.engineer@gmail.com>
2023-09-14 00:16:30 +0900

    :wrench: Test routing

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Blog() {
+  return (
+    <>
+      <h1>Blogページ</h1>
+      <p>ここに記事を書きます</p>
+      <Link href="/">Homeへ</Link>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-xl">
+    <main className="">
       <h1>Dnote</h1>
+      <p>Github Actionsによる自動デプロイを実装</p>
+      <p>Next.jsで実装</p>
+      <Link href="/blog">Blogのページへ</Link>
     </main>
   );
 }
