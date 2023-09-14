7fa4c80ef5654ec7de90553c640b98249536dfd3
Daiki48 <daiki48.engineer@gmail.com>
2023-09-14 15:47:32 +0900

    :zap: Added Panda CSS framework

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+import { css } from "../../styled-system/css";
+// import { css } from "@pandacss/dev";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="">
-      <h1>Dnote</h1>
+      <h1 className={css({ color: "blue", fontSize: "xl" })}>Dnote</h1>
       <p>Github Actionsによる自動デプロイを実装</p>
       <p>Next.jsで実装</p>
+      <div
+        className={css({ fontSize: "2xl", fontWeight: "bold", color: "red" })}
+      >
+        Hello 🐼!
+      </div>
       <Link href="/blog">Blogのページへ</Link>
     </main>
   );
