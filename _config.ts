16850b7c940f7bd6b112d26788757fb31ff7b44f
Daiki48 <daiki48.engineer@gmail.com>
2023-09-17 21:52:28 +0900

    :wrench: Added date plugin

--- _config.ts
+++ _config.ts
@@ -1,7 +1,9 @@
 import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx.ts";
 import sass from "lume/plugins/sass.ts";
+
 import metas from "lume/plugins/metas.ts";
+import date from "lume/plugins/date.ts";
 
 import codeHighlight from "lume/plugins/code_highlight.ts";
 import lang_javascript from "npm:highlight.js/lib/languages/javascript";
@@ -15,6 +17,7 @@ site
   .use(jsx())
   .copy("assets/img")
   .use(sass())
+  .use(date())
   .use(
     codeHighlight({
       languages: {
